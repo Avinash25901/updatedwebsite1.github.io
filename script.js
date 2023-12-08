62bda7d9b560e43e993aa8c015510de018911f94
Avinash <115500130+Avinash25901@users.noreply.github.com>
2023-12-07 20:37:17 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript functionality here if needed
+document.getElementById('play-video').addEventListener('click', function() {
+    // Add code to play video
+    alert('Video playing...');
+});
